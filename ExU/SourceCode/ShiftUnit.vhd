2498f35773770a703c36417dad50e53c35231321
Ryan Kiew <rkiew@sfu.ca>
2020-04-13 18:59:32 -0700

    ShiftUnit FunctionalSim all test cases passed

--- ExU/SourceCode/ShiftUnit.vhd
+++ ExU/SourceCode/ShiftUnit.vhd
@@ -31,23 +31,35 @@ Architecture rtl of ShiftUnit is
   -- Multiplexer Signals
   signal shiftedRightA  : std_logic_vector(N-1 downto 0);
   signal upperShiftedA  : std_logic_vector(N-1 downto 0); 
+  signal upperShiftedAa  : std_logic_vector(N-1 downto 0);
+  signal upperShiftedAb  : std_logic_vector(N-1 downto 0);
   signal lowerShiftedA  : std_logic_vector(N-1 downto 0); 
+  signal lowerShiftedAa  : std_logic_vector(N-1 downto 0); 
+  signal lowerShiftedAb  : std_logic_vector(N-1 downto 0); 
   signal AC             : std_logic_vector(N-1 downto 0);    
 
   signal extendedA      : std_logic_vector(N-1 downto 0);
   signal extendedAC     : std_logic_vector(N-1 downto 0);
+  
+  signal bitshift       : std_logic; -- flag used to signal if a bitshift is required during sign extension
 begin
   -- Extract ShiftCount from B
   shiftCount <= unsigned(B(integer(ceil(log2(real(N)))) - 1 downto 0)); -- lower 6 bits of the register for 64-bit operations
+  with ShiftCount(5) select
+    bitshift <=
+    '0' when '0',
+    '1' when '1',
+    'X' when others;
 
   -- Swapping lower 32 bits to upper
   swappedA(N - 1 downto N/2) <= A(N/2 - 1 downto 0);
-  SwapWordLoop: for index in (N/2 - 1) to 0 generate
-    swappedA(index) <= '0';
-  end generate SwapWordLoop;
+  -- SwapWordLoop: for index in (N/2 - 1) to 0 generate
+    -- swappedA(index) <= '0';
+  -- end generate SwapWordLoop;
+  swappedA(N/2 - 1 downto 0) <= A(N - 1 downto N/2);
 
   -- Mux for A
-  with ShiftFN(1) and ExtWord select
+  with (ShiftFN(1) and ExtWord) select
     tempA <=
       A when '0',
       swappedA when '1',
@@ -72,11 +84,23 @@ begin
       shiftedLL_A when '1',
       (others => 'X') when others;
 
-  -- Sign Extension on shiftedRightA
-  upperShiftedA(N/2 - 1 downto 0) <= shiftedRightA(N/2 - 1 downto 0);
-  sgnext_upper: for i in N-1 downto N/2 generate
-    upperShiftedA(i) <= shiftedRightA(N/2 - 1);
-  end generate sgnext_upper;
+  -- Sign Extension on shiftedRightA (no 32bit shift)
+  upperShiftedAa(N/2 - 1 downto 0) <= shiftedRightA(N/2 - 1 downto 0);
+  sgnext_upper_shift_a: for i in N-1 downto N/2 generate
+	upperShiftedAa(i) <= shiftedRightA(N/2 - 1);
+  end generate sgnext_upper_shift_a;
+  
+  -- Sign Extension on shiftedRightA (with 32bit shift)
+  upperShiftedAb(N/2 - 1 downto 0) <= shiftedRightA(N-1 downto N/2);
+  sgnext_upper_shift_b: for i in N-1 downto N/2 generate
+    upperShiftedAb(i) <= shiftedRightA(N - 1);
+  end generate sgnext_upper_shift_b;
+  
+  with bitshift select
+    upperShiftedA <= 
+	upperShiftedAb when '0',
+	upperShiftedAa when '1',
+	(others => 'X') when others;
   
   -- SgnExt Upper Mux
   with ExtWord select
@@ -85,11 +109,24 @@ begin
     upperShiftedA when '1',
     (others => 'X') when others;
 
-  -- Sign Extension on AC
-  lowerShiftedA(N/2 - 1 downto 0) <= AC(N/2 - 1 downto 0);
-  sgnext_lower: for i in N-1 downto N/2 generate
-    lowerShiftedA(i) <= AC(N/2 - 1);
-  end generate sgnext_lower;
+  -- Sign Extension on AC (no 32bit shift)
+  lowerShiftedAa(N/2 - 1 downto 0) <= AC(N/2 - 1 downto 0);
+  sgnext_lower_shift_a: for i in N-1 downto N/2 generate
+	lowerShiftedAa(i) <= AC(N/2 - 1);
+  end generate sgnext_lower_shift_a;
+  
+  -- Sign Extension on AC (with 32bit shift)
+  lowerShiftedAb(N/2 - 1 downto 0) <= AC(N-1 downto N/2);
+  sgnext_lower_shift_b: for i in N-1 downto N/2 generate
+    lowerShiftedAb(i) <= AC(N - 1);
+  end generate sgnext_lower_shift_b;
+  
+  with bitshift select
+    lowerShiftedA <= 
+	lowerShiftedAa when '0',
+	lowerShiftedAb when '1',
+	(others => 'X') when others;
+
 
   -- SgnExt Lower Mux
   with ExtWord select
