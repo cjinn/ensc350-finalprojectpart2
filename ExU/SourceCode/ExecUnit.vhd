3df92267eb192b287509d8311e3b1a379a1740dd
Choong Jin Ng <jin8383@gmail.com>
2020-04-16 07:05:05 -0700

    Rewrote ExecUnit.vhd for better readability

--- ExU/SourceCode/ExecUnit.vhd
+++ ExU/SourceCode/ExecUnit.vhd
@@ -1,7 +1,11 @@
 -----------------------------------------------------------------------------
--- ExecUnit
+-- ExecUnit.vhd
+-- VHDL code of a 64-bit Execution Unit
 -----------------------------------------------------------------------------
 
+-----------------------------------------------------------------------------
+-- ExecUnit Declaraction
+-----------------------------------------------------------------------------
 library ieee;
 Use ieee.std_logic_1164.all;
 Use ieee.std_logic_arith.all;
@@ -9,56 +13,72 @@ Use ieee.std_logic_arith.all;
 Entity ExecUnit is
   Generic ( N : natural := 64 );
   Port (
-    A, B              : in std_logic_vector( N-1 downto 0 );
-    NotA              : in std_logic := '0';
-    FuncClass         : in std_logic_vector( 1 downto 0 );
-    LogicFN           : in std_logic_vector( 1 downto 0 );
-    ShiftFN           : in std_logic_vector( 1 downto 0 );
-    AddnSub, ExtWord  : in std_logic := '0';
-    
-    Y                 : out std_logic_vector( N-1 downto 0 );
-    Zero, AltB, AltBu : out std_logic);
+    -- Input Signals
+    A                 : in std_logic_vector( N-1 downto 0 );  -- 64-bit number A
+    B                 : in std_logic_vector( N-1 downto 0 );  -- 64-bit number B
+    NotA              : in std_logic := '0';                  -- If '1', sets A to NOT(A)
+    FuncClass         : in std_logic_vector( 1 downto 0 );    -- Selects which general function you want to use   
+    LogicFN           : in std_logic_vector( 1 downto 0 );    -- Selects which logic function to use
+    ShiftFN           : in std_logic_vector( 1 downto 0 );    -- Selects which shifting function to use
+    AddnSub           : in std_logic := '0';                  -- Specifies whether adding (B is positive) or subtracting (B is negative), '1' and '0' respectively
+    ExtWord           : in std_logic := '0';                  -- If '1', sign extend result
+
+    -- Output Signals
+    Y                 : out std_logic_vector( N-1 downto 0 ); -- Result of Y
+    Zero, AltB, AltBu : out std_logic);                       -- Adder-related flags
 End Entity ExecUnit;
 
 Architecture rtl of ExecUnit is
   -- ArithUnit Signals
-  signal AdderAddY     : std_logic_vector(N-1 downto 0);
-  signal AdderResult   : std_logic_vector(N-1 downto 0);
-  signal AdderCout     : std_logic;
-  signal AdderOvfl     : std_logic;
+  signal AdderAddY      : std_logic_vector(N-1 downto 0);
+  signal AdderResult    : std_logic_vector(N-1 downto 0);
+  signal AdderCout      : std_logic;
+  signal AdderOvfl      : std_logic;
 
   -- Shift Signals
-  signal ShiftResult   : std_logic_vector(N-1 downto 0);
+  signal ShiftResult    : std_logic_vector(N-1 downto 0);
 
   -- Logic Signals
-  signal LogicResult   : std_logic_vector(N-1 downto 0);
+  signal LogicResult    : std_logic_vector(N-1 downto 0);
 
-  -- AltB and AltBu before
-  signal AltBuSignal   : std_logic;
-  signal AltBSignal    : std_logic;
+  -- AltB and AltBu output from Adder
+  signal AltBuSignal    : std_logic;
+  signal AltBSignal     : std_logic;
+
+  -- Sign Extended
+  signal AltBuSignalExt : std_logic_vector(N-1 downto 0) := (others => '0');
+  signal AltBSignalExt  : std_logic_vector(N-1 downto 0) := (others => '0');
 begin
   -- Arithmetic Unit
   AU: entity work.ArithUnit generic map (N) port map 
-    (A, B, AddnSub, NotA, ExtWord, 
-    AdderAddY, AdderResult, AdderCout, AdderOvfl, Zero, AltBSignal, AltBuSignal);
+    (A, B, AddnSub, NotA, ExtWord,                                                -- Inputs
+    AdderAddY, AdderResult, AdderCout, AdderOvfl, Zero, AltBSignal, AltBuSignal); -- Outputs
 
   -- Shift Unit
   SU: entity work.ShiftUnit generic map (N) port map
-    (A, B, AdderResult, ShiftFN, ExtWord, ShiftResult);
+    (A, B, AdderResult, ShiftFN, ExtWord,                                         -- Inputs
+    ShiftResult);                                                                 -- Outputs
 
   -- Logic Unit
   LU: entity work.LogicUnit generic map (N) port map
-    (A, B, LogicFn, LogicResult);
+    (A, B, LogicFn,                                                               -- Inputs
+    LogicResult);                                                                 -- Outputs
 
-  -- Pick which results from which unit with a mux
-  with FuncClass select
-    Y <=
-      ((N-1 downto 1 => '0')&AltBuSignal) when "00",
-      ((N-1 downto 1 => '0')&AltBSignal) when "01",
-      ShiftResult when "10",
-      LogicResult when "11",
-      (others => 'X') when others;
+  -- Sign Extension for AltBSignal and AltBuSignal
+  AltBuSignalExt <= ((N-1 downto 1 => '0')&AltBuSignal);
+  AltBSignalExt <= ((N-1 downto 1 => '0')&AltBSignal);
 
+  -- MUX to selects the following results depending on FuncClass
+  -- If FuncClass = "00", Y <= ((N-1 downto 1 => '0')&AltBuSignal)
+  -- If FuncClass = "01", Y <= ((N-1 downto 1 => '0')&AltBSignal)
+  -- If FuncClass = "10", Y <= ShiftResult
+  -- If FuncClass = "11", Y <= LogicResult
+  FuncClassMux  : entity Work.MUX4bit generic map(N) port map(
+    AltBuSignalExt,                       -- TODO: Add comment
+    AltBSignalExt,                        -- TODO: Add comment
+    ShiftResult,                          -- Result from ShiftUnit
+    LogicResult,                          -- Result from LogicUnit
+    FuncClass, Y);                        -- Select signal and output respectively
   
   -- Assigning to Output
   AltB <= AltBSignal;
